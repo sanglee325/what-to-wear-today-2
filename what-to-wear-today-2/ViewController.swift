6db35915eea8c458c99ea81b010ab4860ffe617a
sanglee <pksll325@gmail.com>
2020-05-30 16:58:11 +0900

    Initial Commit

---
+++ what-to-wear-today-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  what-to-wear-today-2
+//
+//  Created by 박 상리 on 2020/05/30.
+//  Copyright © 2020 sanglee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
