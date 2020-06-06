45177244afa3a55dec44b1e5b485cb6817e5e755
Sejin Kim <lemon05090@gmail.com>
2020-06-06 21:44:52 +0900

    make ClosetViewController/DiaryViewController

---
+++ what-to-wear-today-2/ClosetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ClosetViewController.swift
+//  
+//
+//  Created by 김세진 on 2020/06/06.
+//
+
+import UIKit
+
+class ClosetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
